962b7e161f40b9b88028dfd006f7231f29603d6b
d4ni5hhh <40337069+d4ni5hhh@users.noreply.github.com>
2024-02-19 13:31:11 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,25 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <script>
+        var appInventorInput = window.AppInventor.getWebViewString();
+        document.querySelectorAll("a").innerHtml = studentData;
+    </script>
+    <table>
+        <a></a>
+        <tr>
+            <th>Nama</th>
+            <th>Case</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>2</td>
+        </tr>
+    </table>
+</body>
+</html>
 
